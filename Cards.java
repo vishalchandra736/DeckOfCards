f90afbba32bf5946e9915d082cd060ff061dae03
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-04 11:19:47 +0530

    Added : Program of Deck of cards

---
+++ Cards.java
@@ -0,0 +1,20 @@
+package com.blz.dackofcards;
+
+import java.util.LinkedList;
+
+public class Cards {
+    LinkedList<String> cards = new LinkedList<String>();
+    String[] suits = { "Diamond", "Heart", "Spades", "Club" };
+    String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+    public void setCards() {
+        for(int i = 0; i <= 3; i++) {
+            for(int j = 0; j <= 12; j++ ) {
+                cards.add(suits[i]+" "+rank[j]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Cards card = new Cards();
+        card.setCards();
+    }
+}
